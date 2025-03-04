9eab03f4ec0d3244b858ee90b3864863b261e028
maxpepperdine <mpepperdine@ucsb.edu>
2025-03-04 15:50:43 -0800

    final portfolio commits!

--- scripts/mark_recapture_m_n_m.R
+++ scripts/mark_recapture_m_n_m.R
@@ -20,6 +20,9 @@ NPetersen(n1, n2, m2) # estimates the population size
 ciPetersen(n1,n2,m2)  # 95% CIs using normal approximation ($ciNorm) and bootstrapping ($ciBoot)
 
 
+--------------------------------------------------------------------------------
+
+  
 # Example from lecture notes
 #Sass et al. (2010) Silver carp study
 n1_sc <- 4540
@@ -29,26 +32,42 @@ m2_sc <- 30
 NChapman(n1_sc, n2_sc, m2_sc)
 ciChapman(n1_sc, n2_sc, m2_sc)
 
-NPetersen(n1, n2, m2) 
-ciPetersen(n1,n2,m2)  
 
 #PORTFOLIO ASSIGNMENT
 # Run the Petersen Estimator for the  Silver Carp parameters (APPLY FUNCTIONS ABOVE). 
+NPetersen(n1_sc, n2_sc, m2_sc) 
+ciPetersen(n1_sc, n2_sc, m2_sc)  
+
+## What is the difference between the Petersen and Chapman estimates and the confidence intervals (EXPLAIN)
+
+##########
+# ANSWER
+#########
+
+# Quantitative difference between the Petersen and Chapman estimates
+NPetersen(n1_sc, n2_sc, m2_sc) - NChapman(n1_sc, n2_sc, m2_sc)
+
+## Explanation of the difference
+# Both estimates are simple and often used with large sample sizes, however the 
+# mathematical formula for each estimator is different. The Chapman estimate 
+# accounts for variance and sample bias, whereas the Petersen estimate does not.
+# The bootstrapped CI for the Petersen estimate (~247,928 - 508,253) is wider 
+# than the bootstrapped Chapman CI (~215,611 - 440,634).
 
 
-# What is the difference between the Petersen and Chapman estimates and the confidence intervals (EXPLAIN)
+--------------------------------------------------------------------------------
 
-
+  
 # How many M&Ms are in this bag? (WRITE YOUR OWN CODE)
-n1 <- 78 # number of individuals marked in the first sampling
-n2 <- 168 # number of individuals captured in the second sampling effort
-m2 <- 19 # number of individuals in the second sampling effort that were marked.  
+n1_m <- 78 # number of individuals marked in the first sampling
+n2_m <- 168 # number of individuals captured in the second sampling effort
+m2_m <- 19 # number of individuals in the second sampling effort that were marked.  
 
-NPetersen(n1, n2, m2) # estimated the population size
-ciPetersen(n1,n2,m2)  # 95% CIs using normal approximation ($ciNorm) and bootstrapping ($ciBoot)
+NPetersen(n1_m, n2_m, m2_m) # estimated the population size
+ciPetersen(n1_m, n2_m, m2_m)  # 95% CIs using normal approximation ($ciNorm) and bootstrapping ($ciBoot)
 
-NChapman(n1, n2, m2)
-ciChapman(n1, n2, m2)
+NChapman(n1_m, n2_m, m2_m)
+ciChapman(n1_m, n2_m, m2_m)
 
 
 
